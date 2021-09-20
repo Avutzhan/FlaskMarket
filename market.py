ec866b5dc4601be7f93ed4a3da55eda4cb6311c6
Avutzhan <dautov92@list.ru>
2021-09-20 14:17:20 +0600

    * send dict to template * add market table

--- market.py
+++ market.py
@@ -11,6 +11,11 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    return render_template('market.html', item_name='Phone')
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '879879789789788', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123879789789788', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '345879789789788', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
 
 
