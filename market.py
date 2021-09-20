ea6c423533ce33b6d3a3f7a5d846930962eb5289
Avutzhan <dautov92@list.ru>
2021-09-20 12:53:07 +0600

    add simple template

--- market.py
+++ market.py
@@ -1,18 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, world!!!</h1>'
+def home_page():
+    return render_template('home.html')
 
-
-@app.route('/about')
-def about_page():
-    return '<h1>About Page</h1>'
-
-
-@app.route('/about/<username>')
-def about_profile(username):
-    return f'<h1>This is About Page of {username}</h1>'
