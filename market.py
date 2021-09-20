c27fa9ef845822e945b534a711cf82e0f224dd14
Avutzhan <dautov92@list.ru>
2021-09-20 13:57:26 +0600

    add market route * send data to template * use jinja template to get data from handler

--- market.py
+++ market.py
@@ -8,3 +8,9 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_name='Phone')
+
+
