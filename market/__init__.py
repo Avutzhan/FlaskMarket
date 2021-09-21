7f2b8371ffe775e22faebc5e4d09056df0f2197f
Avutzhan <dautov92@list.ru>
2021-09-21 17:26:16 +0600

    * model relationships * forms

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '48afdc47725c74c0d4441e5d'
 db = SQLAlchemy(app)
 
 from market import routes
