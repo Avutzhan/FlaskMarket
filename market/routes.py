ccf9f32924f15ca3aec5bbdf1cd04924230c64c2
Avutzhan <dautov92@list.ru>
2021-09-23 11:51:36 +0600

    * flash messages * advanced validation

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -29,5 +29,5 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print('There is error with creating user: ' + err_msg[0])
+            flash('There is error with creating user: ' + err_msg[0], category='danger')
     return render_template('register.html', form=form)
