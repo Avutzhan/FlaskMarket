21afe5aba6f8a0b58a2cd9a10424216b9cbb5122
Avutzhan <dautov92@list.ru>
2021-09-27 09:59:13 +0600

    * login * budget badge * font awesome cdn * logout

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '48afdc47725c74c0d4441e5d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
